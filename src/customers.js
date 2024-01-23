00b242fbfe8d8672535881b5b03fe56deeb7eb8a
Michael Lazarski <michael.lazarski@scania.com>
2024-01-23 21:29:29 +0100

    hello customers

--- src/customers.js
+++ src/customers.js
@@ -1,11 +1,3 @@
-Customers.prototype.UpdateBillingInfo = function(info) {
-    this.cc = info.cc;
-    this.type = info.type;
-    this.name = info.name;
-    this.expM = info.expM;
-    this.expY = info.expY;
-    this.ccv = info.ccv;
-
-    /* snip a bunch of similar lines */
-    this.saveToDatabase();
+Customers.prototype.hello = function() {
+    console.log("Hello");
 };
