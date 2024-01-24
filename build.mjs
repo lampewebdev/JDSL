256420e2043e0c713f71126778af9e4e36ca9b4b
Michael Lazarski <michael.lazarski@scania.com>
2024-01-24 21:56:51 +0100

    JDSL can now load json fiels and will create a server.ls file which can be run

--- build.mjs
+++ build.mjs
@@ -1,5 +1,6 @@
 import { simpleGit } from 'simple-git';
 import fs from 'node:fs/promises';
+import path from 'node:path';
 
 async function cloneRepo() {
     const option = {
@@ -11,23 +12,46 @@ async function cloneRepo() {
     await git.clone("git@github.com:lampewebdev/JDSL.git")
 }
 
-async function main() {
-    await fs.rm("/tmp/JDSL", { recursive: true, force: true });
-    await cloneRepo();
+async function find_all_json() {
+    const files = await fs.readdir('./src/');
+    return files.filter(file => path.extname(file) === '.json');
+}
+
+async function git_worker() {
     const option = {
         baseDir: "/tmp/JDSL",
         binary: 'git',
     };
 
-    const git = simpleGit(option);
-    const data = await fs.readFile('./src/customers.json', 'utf8');
-    const jsonData = JSON.parse(data);
-    const someHash = jsonData.Functions[0];
-    await git.checkout(someHash);
-    const customerFile = await fs.readFile('/tmp/JDSL/src/customers.js', 'utf8');
-    fs.appendFile('./dist/customers.js', customerFile);
-    // await fs.rm("./dist/", { recursive: true, force: true });
-    // await fs.mkdir("./dist/");
+    return simpleGit(option);
+}
+
+async function create_class(git, jsonData) {
+
+    await fs.appendFile('./dist/server.js', "class " + jsonData.Class + "{};");
+    for (const funcHash in jsonData.Functions) {
+        await git.checkout(jsonData.Functions[funcHash]);
+        const customerFile = await fs.readFile('/tmp/JDSL/src/' + jsonData.File, 'utf8');
+        await fs.appendFile('./dist/server.js', customerFile);
+    }
+}
+
+async function main() {
+    await fs.rm("./dist/", { recursive: true, force: true });
+    await fs.mkdir("./dist/");
+
+    await fs.rm("/tmp/JDSL", { recursive: true, force: true });
+    await cloneRepo();
+    const git = await git_worker();
+    const jsonFiles = await find_all_json();
+    for (const jsonFile of jsonFiles) {
+        const data = await fs.readFile('./src/' + jsonFile, 'utf8');
+        const jsonData = JSON.parse(data);
+        await create_class(git, jsonData);
+    };
+
+    const serverFile = await fs.readFile("./server.js");
+    await fs.appendFile("./dist/server.js", serverFile);
 }
 
 main();
---
+++ server.js
@@ -0,0 +1,4 @@
+(function run() {
+    const customer = new Customers();
+    console.log(customer.hello(), customer.world())
+})();
